e51296d0dd9d2d188ce59eaad76ae0462d282ff5
Sakshi-Kalamb <555kalambsakshi@gmail.com>
2025-07-25 17:07:10 +0530

    all files uploaded

---
+++ audino code.ino
@@ -0,0 +1,39 @@
+/*
+  AnalogReadSerial
+
+  Reads an analog input on pin 0, prints the result to the Serial Monitor.
+  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
+  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/AnalogReadSerial
+*/
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  pinMode(A0,INPUT_PULLUP);
+  pinMode(13,OUTPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  // print out the value you read:
+  Serial.println(sensorValue);
+  delay(100);  
+  if(sensorValue<=500)
+  { 
+    digitalWrite(13,0);
+  Serial.println("light");
+  }
+  else
+  {
+    Serial.println("dark");
+  digitalWrite(13,1);
+  }
+  // delay in between reads for stability
+}
